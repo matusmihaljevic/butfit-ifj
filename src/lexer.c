f7afaa9020a7b866bd0c2a7fac8353da2f654337
Jaroslav Podmajersky <jarkopodmajersky013@gmail.com>
2024-09-26 15:00:51 +0200

    Zakladna kostra projektu

---
+++ src/lexer.c
@@ -0,0 +1,12 @@
+/**
+ * @file parser.h
+ * 
+ * Lexikálny analyzátor 
+ * 
+ * IFJ Projekt 2024
+ * 
+ * @author <xbojnaa00> Adam Bojnanský
+ * @author <xmihalm00> Matúš Mihaljevič
+ * @author <xpribik00> Kristián Pribila
+ * @author <xpodmaj00> Jaroslav Podmajerský
+ */
