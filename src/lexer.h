8a78b993845bbc5e89d828a74807fb54e90d7648
root <root@DESKTOP-KM1UDRH>
2024-09-26 15:00:51 +0200

    Zakladna kostra projektu

---
+++ src/lexer.h
@@ -0,0 +1,14 @@
+/**
+ * @file parser.h
+ * 
+ * Lexikálny analyzátor (hlavičkový súbor)
+ * 
+ * IFJ Projekt 2024
+ * 
+ * @author <xbojnaa00> Adam Bojnanský
+ * @author <xmihalm00> Matúš Mihaljevič
+ * @author <xpribik00> Kristián Pribila
+ * @author <xpodmaj00> Jaroslav Podmajerský
+ */
+
+#pragma once
