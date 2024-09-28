017a0e6c0e301c2598d4427d1184382bf94d4d87
xmihalm00 <xmihalm00@stud.fit.vutbr.cz>
2024-09-28 23:13:52 +0200

    create token representation

---
+++ token.h
@@ -0,0 +1,101 @@
+/**
+ * @file
+ * @brief Error definitions.
+ * @author Matúš Mihaljevič <xmihalm00@stud.fit.vutbr.cz>
+ */
+
+
+#ifndef _TOKEN_H
+#define _TOKEN_H
+
+/**
+ * @enum Reserved keywords.
+ */
+typedef enum
+{
+    KEYWORD_CONST,    // const
+    KEYWORD_ELSE,     // else
+    KEYWORD_FN,       // fn
+    KEYWORD_IF,       // if
+    KEYWORD_I32,      // i32
+    KEYWORD_F64,      // f64
+    KEYWORD_U8,       // u8
+    KEYWORD_NULL,     // null
+    KEYWORD_PUB,      // pub
+    KEYWORD_RETURN,   // return
+    KEYWORD_VAR,      // var
+    KEYWORD_VOID,     // void
+    KEYWORD_WHILE     // while
+} Keyword;
+
+/**
+ * @enum Type of token.
+ */
+typedef enum
+{
+	TOKEN_TYPE_EOF, // End of file
+	TOKEN_TYPE_EOL, // End of line
+	TOKEN_TYPE_EMPTY, // Empty
+
+	TOKEN_TYPE_IDENTIFIER, // Identifier
+	TOKEN_TYPE_KEYWORD, // Keyword
+	TOKEN_TYPE_INTERN, // Intern function
+
+	TOKEN_TYPE_INT32, // Integer number
+	TOKEN_TYPE_FLOAT64, // Double number
+	TOKEN_TYPE_U8, // String
+
+	TOKEN_TYPE_EQ, // Equal ==
+	TOKEN_TYPE_NEQ, // Not equal !=
+	TOKEN_TYPE_LEQ, // Less or equal <=
+	TOKEN_TYPE_LTN, // Less than <
+	TOKEN_TYPE_MEQ, // More or equal >=
+	TOKEN_TYPE_MTN, // More than >
+
+	TOKEN_TYPE_ASSIGN, // Assign =
+	TOKEN_TYPE_PLUS, // Plus +
+	TOKEN_TYPE_MINUS, // Minus -
+	TOKEN_TYPE_MUL, // Multiplication *
+	TOKEN_TYPE_DIV, // Division /
+
+	TOKEN_TYPE_LEFT_BRACKET, // Left bracket (
+	TOKEN_TYPE_RIGHT_BRACKET, // Right bracket )
+	TOKEN_TYPE_LEFT_BRACE, // Left brace {
+	TOKEN_TYPE_RIGHT_BRACE, // Right brace }
+
+	TOKEN_TYPE_COMMA, // Comma ,
+	TOKEN_TYPE_COLON, // Colon :
+	TOKEN_TYPE_DOT, // Dot .
+	TOKEN_TYPE_SEMICOLON, // Semicolon ;
+} Token_type;
+
+/**
+ * @union Token attribute.
+ */
+typedef union
+{
+    int i32; // Integer value.
+	Keyword keyword; // Keyword, one of the KEYWORD_... constant
+	double f64; // Decimal value.
+} Token_attribute;
+
+
+typedef struct
+{
+    const unsigned int code;
+    const char *message;
+} Token_error;
+
+/**
+ * @struct Representation of a token.
+ */
+typedef struct
+{
+    const char *lexeme;
+	Token_type type; // Token type, one of token_type constants.
+	Token_attribute attribute; // Attribute of token.
+	Token_error error; // Token error.
+    const unsigned int line;
+} Token;
+
+#endif //_TOKEN_H
