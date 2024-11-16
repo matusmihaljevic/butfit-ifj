c131ffbf3c19879d0ad7df9cfdf454aa5336e315
JPod101010 <jarkopodmajersky013@gmail.com>
2024-11-16 08:46:29 +0100

    modifikacia mainu

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include "scanner.h"
 #include "parser.h"
 #include "error.h"
+#include "codegen.h"
+#include "symtable.h"
+#include "semantic.h"
 #include <stdio.h>
 
 int main() {
+	
 	set_source_file(stdin);
-
 	ASTNode *root = parse_program();
-	print_ast(root, 0, false, true);
-
+	//print_ast(root,0,0,false);
+	check_semantic_program(root);
+	char* output = generate_program(root);
+	fprintf(stdout,"%s",output);
     return 0;
 }
