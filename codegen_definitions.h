e4fef2400a631f415bfabfbb10770ffff795a617
JPod101010 <jarkopodmajersky013@gmail.com>
2024-11-26 19:37:22 +0100

    uprava definicii pre hash

--- codegen_definitions.h
+++ codegen_definitions.h
@@ -1,11 +1,11 @@
 /**
  * Project: Implementation of the IFJ24 imperative language compiler.
- * 
+ *
  * @file codegen_definitions.h
  * @brief Header file for code definitions used in the program.
- * 
+ *
  * This file provides all definitions used in codegen.c
- * 
+ *
  * @author Jaroslav Podmajerský
  */
 
@@ -36,53 +36,50 @@
 
 #define INTERN_FN_LENGHT DString_concat(&Output,"POPS GF@GF_RESULT",                   \
                                         "\nSTRLEN GF@GF_RESULT GF@GF_RESULT",          \
-                                        "\nPUSHS GF@GF_RESULT\n",NULL);                         
+                                        "\nPUSHS GF@GF_RESULT\n",NULL);
 
 #define INTERN_FN_CONCAT DString_concat(&Output,"POPS GF@GF_OPERAND2\nPOPS GF@GF_OPERAND1\n"            \
                                                 "CONCAT GF@GF_RESULT GF@GF_OPERAND1 GF@GF_OPERAND2\n"   \
-                                                "PUSHS GF@GF_RESULT\n",NULL);                                    
+                                                "PUSHS GF@GF_RESULT\n",NULL);
 
-#define INTERN_FN_STRCMP char* curr_id = id++;                                                    \
-                        DString_concat(&Output,"POPS GF@GF_OPERAND2\nPOPS GF@GF_OPERAND1\n"       \
-                                               "JUMPIFEQ $str_eq_",&curr_id," GF@GF_OPERAND1 GF@GF_OPERAND2\n"                  \
-                                               "LT GF@GF_RESULT GF@GF_OPERAND1 GF@GF_OPERAND2\n"                                \
-                                               "JUMPIFEQ $str_less_",&curr_id," GF@GF_RESULT bool@true\n"                       \
-                                               "PUSHS int@1\nJUMP $str_cmp_end_",&curr_id,"\n"                                  \
-                                               "LABEL $str_less_",&curr_id,"\nPUSHS int@-1\nJUMP $str_cmp_end_",&curr_id,"\n"   \
-                                               "LABEL $str_eq_",&curr_id,"\nPUSHS int@0\nLABEL $str_cmp_end_",&curr_id,"\n",    \
-                                               NULL); 
+#define INTERN_FN_STRCMP DString_concat(&Output,"POPS GF@GF_OPERAND2\nPOPS GF@GF_OPERAND1\n"       \
+                                               "JUMPIFEQ $str_eq_",get_CB_hash(built_in_fn_node)," GF@GF_OPERAND1 GF@GF_OPERAND2\n"                  \
+                                               "LT GF@GF_RESULT GF@GF_OPERAND1 GF@GF_OPERAND2\n"                                					 \
+                                               "JUMPIFEQ $str_less_",get_CB_hash(built_in_fn_node)," GF@GF_RESULT bool@true\n"                       \
+                                               "PUSHS int@1\nJUMP $str_cmp_end_",get_CB_hash(built_in_fn_node),"\n"                                  				      \
+                                               "LABEL $str_less_",get_CB_hash(built_in_fn_node),"\nPUSHS int@-1\nJUMP $str_cmp_end_",get_CB_hash(built_in_fn_node),"\n"   \
+                                               "LABEL $str_eq_",get_CB_hash(built_in_fn_node),"\nPUSHS int@0\nLABEL $str_cmp_end_",get_CB_hash(built_in_fn_node),"\n",    \
+                                               NULL);
 
-
-#define INTERN_FN_SUBSTR    char* curr_id = id++;                                    \
-                            DString_concat(&Output,"POPS GF@GF_OPERAND2\n"           \
+#define INTERN_FN_SUBSTR DString_concat(&Output,"POPS GF@GF_OPERAND2\n"           \
                                         "POPS GF@GF_OPERAND1\n"                      \
                                         "LT GF@GF_RESULT GF@GF_OPERAND1 int@0\n"     \
-                                        "JUMPIFEQ $ret_nil_",&curr_id," GF@GF_RESULT bool@true\n"    \
+                                        "JUMPIFEQ $ret_nil_",get_CB_hash(built_in_fn_node)," GF@GF_RESULT bool@true\n"    \
                                         "LT GF@GF_RESULT GF@GF_OPERAND2 int@0\n"                     \
-                                        "JUMPIFEQ $ret_nil_",&curr_id," GF@GF_RESULT bool@true\n"    \
+                                        "JUMPIFEQ $ret_nil_",get_CB_hash(built_in_fn_node)," GF@GF_RESULT bool@true\n"    \
                                         "GT GF@GF_RESULT GF@GF_OPERAND1 GF@GF_OPERAND2\n"            \
-                                        "JUMPIFEQ $ret_nil_",&curr_id," GF@GF_RESULT bool@true\n"    \
+                                        "JUMPIFEQ $ret_nil_",get_CB_hash(built_in_fn_node)," GF@GF_RESULT bool@true\n"    \
                                         "POPS GF@GF_SUBSTR\n"        \
                                         "STRLEN GF@GF_RESULT GF@GF_SUBSTR\n"    \
                                         "LT GF@GF_RESULT GF@GF_RESULT GF@GF_OPERAND2\n"              \
-                                        "JUMPIFEQ $ret_nil_",&curr_id," GF@GF_RESULT bool@true\n"    \
+                                        "JUMPIFEQ $ret_nil_",get_CB_hash(built_in_fn_node)," GF@GF_RESULT bool@true\n"    \
                                         "STRLEN GF@GF_RESULT GF@GF_SUBSTR\n"                         \
                                         "GT GF@GF_RESULT GF@GF_RESULT GF@GF_OPERAND1\n"              \
                                         "NOT GF@GF_RESULT GF@GF_RESULT\n"                            \
-                                        "JUMPIFEQ $ret_nil_",&curr_id," GF@GF_RESULT bool@true\n"    \
+                                        "JUMPIFEQ $ret_nil_",get_CB_hash(built_in_fn_node)," GF@GF_RESULT bool@true\n"    \
                                         "MOVE GF@GF_SBSTR_O string@\n"                  \
-                                        "LABEL $while_substr_",&curr_id,"\n"            \
-                                        "JUMPIFEQ $end_while_substr_",&curr_id," GF@GF_OPERAND1 GF@GF_OPERAND2\n"    \
+                                        "LABEL $while_substr_",get_CB_hash(built_in_fn_node),"\n"            \
+                                        "JUMPIFEQ $end_while_substr_",get_CB_hash(built_in_fn_node)," GF@GF_OPERAND1 GF@GF_OPERAND2\n"    \
                                         "GETCHAR GF@GF_RESULT GF@GF_SUBSTR GF@GF_OPERAND1\n"                       \
                                         "CONCAT GF@GF_SBSTR_O GF@GF_SBSTR_O GF@GF_RESULT\n"                        \
                                         "ADD GF@GF_OPERAND1 GF@GF_OPERAND1 int@1\n"     \
-                                        "JUMP $while_substr_",&curr_id,"\n"             \
-                                        "LABEL $end_while_substr_",&curr_id,"\n"        \
+                                        "JUMP $while_substr_",get_CB_hash(built_in_fn_node),"\n"             \
+                                        "LABEL $end_while_substr_",get_CB_hash(built_in_fn_node),"\n"        \
                                         "PUSHS GF@GF_SBSTR_O\n"                         \
-                                        "JUMP $end_substr_",&curr_id,"\n"               \
-                                        "LABEL $ret_nil_",&curr_id,"\n"                 \
-                                        "PUSHS nil@nil\n"                               \
-                                        "LABEL $end_substr_",&curr_id,"\n",             \
+                                        "JUMP $end_substr_",get_CB_hash(built_in_fn_node),"\n"               \
+                                        "LABEL $ret_nil_",get_CB_hash(built_in_fn_node),"\n"                 \
+                                        "PUSHS nil@nil\n"                               					 \
+                                        "LABEL $end_substr_",get_CB_hash(built_in_fn_node),"\n",             \
                                         NULL);
 
 #define INTERN_FN_ORD   DString_concat(&Output,"POPS GF@GF_OPERAND1\nPOPS GF@GF_RESULT\n",        \
